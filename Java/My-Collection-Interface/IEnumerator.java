a63207e08710b26cb81fcce2717675b87265e5f2
Chris Medina <medichri87@hotmail.com>
2014-12-13 22:13:31 -0500

    IEnumerator.java

---
+++ Java/My-Collection-Interface/IEnumerator.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ICollection;
+
+/**
+ * @param <E>
+ * @date Jul 26, 2014
+ * @author Chris Medina
+ *
+ * <p>
+ * IEnumerator provides for linear traversal of any class implement
+ * ICollection</p>
+ */
+public interface IEnumerator<E> {
+
+    /**
+     * Determine if there is another item after the current one to traverse to
+     *
+     * @return true, if there is another item to move forward to
+     */
+    boolean hasNext();
+
+    /**
+     * Moves the iterator forward one item assuming there is another item to
+     * move forward to
+     *
+     * @return the next item in the iterator
+     */
+    E next();
+
+    /**
+     * Set the iterator back to the beginning
+     */
+    void reset();
+}
